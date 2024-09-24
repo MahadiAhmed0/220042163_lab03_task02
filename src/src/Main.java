096e946ea00c4fb8ab055d5226e64f436281bcd9
Mahadi <mahadiahmed.2002@gmail.com>
2024-09-24 20:11:28 +0600

    added MinStack Class -- updated main

---
+++ src/src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(3);
+        minStack.push(2);
+        minStack.push(5);
+        minStack.push(1);
+        minStack.pop();
+        System.out.println(minStack.min());
+
+        System.out.println("\n");
+    }
+}
